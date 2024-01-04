41bccbc4f0b969d3c333acf2e5bbbe8225d573b6
Akshay26953 <amkharat2695@gmail.com>
2024-01-04 14:15:12 +0530

    Feat: Add extra runs to local storage and fetch score on screen

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const showRuns = document.querySelector(".showRuns");
+const showOvers = document.querySelector(".showOvers");
+
+function showScore() {
+  const score = JSON.parse(localStorage.getItem("scoreCard"));
+  showRuns.innerHTML = score.score + " / " + score.wickets;
+
+  if (score.over.length < 6) {
+    showOvers.innerHTML =
+      score.overs.length - 1 + "." + score.over.length + " Overs";
+  } else {
+    showOvers.innerHTML = score.overs.length + "." + "0 Overs";
+  }
+}
+showScore();
+
 function addRun(run) {
   updateOver(run);
 }
@@ -33,16 +49,64 @@ function updateOvers(run, score) {
 
 function updateScore(run, score) {
   if (typeof run === "number") {
-    score.target += run;
+    score.score += run;
   } else if (run === "W") {
     score.wickets += 1;
   } else {
     score.extras += 1;
-    score.target += 1;
+    score.score += 1;
   }
   localStorage.setItem("scoreCard", JSON.stringify(score));
+  showScore();
 }
 
 function addExtraRun(extraRun) {
-  
+  const score = JSON.parse(localStorage.getItem("scoreCard"));
+  if (extraRun === 0) {
+    run = score.over.pop();
+    console.log(run);
+  } else {
+    run = extraRun + "+" + score.over.pop();
+  }
+  score.over.push(run);
+  score.overs.pop();
+  score.overs.push(score.over);
+  score.score += extraRun;
+  localStorage.setItem("scoreCard", JSON.stringify(score));
+  showScore();
+}
+
+function resetLast() {
+  const score = JSON.parse(localStorage.getItem("scoreCard"));
+  console.log(score.over.length);
+  if (score.over.length !== 0) {
+    const lastScore = score.over.pop();
+    if (typeof lastScore === "number") {
+      score.score -= lastScore;
+    } else {
+      if (lastScore[0] === "W" || lastScore[2] === "W") {
+        score.wickets -= 1;
+        if (lastScore[2] === "W") {
+          score.score -= Number(lastScore[0]);
+        }
+      } else {
+        score.extras -= 1;
+        if (lastScore[2] === "w" || lastScore[2] === "n") {
+          score.score -= Number(lastScore[0]) + 1;
+        }
+      }
+    }
+    score.overs.pop();
+    score.overs.push(score.over);
+    localStorage.setItem("scoreCard", JSON.stringify(score));
+  }
+  showScore();
+}
+
+function resetAll() {
+  const reset = confirm("Do you want to start fresh?");
+  if (reset) {
+    localStorage.clear();
+  }
+  showScore();
 }
